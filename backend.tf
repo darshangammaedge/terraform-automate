ba02cec09f9733db4af45c3c14e82e08e666f5d3
darshangammaedge <darshan.patidar@gammaedge.io>
2026-01-30 12:02:11 +0530

    second commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-s3-bucket-darshan"
+    bucket         = "terraform-s3-bucket-darshan-1"
     key            = "dev/terraform.tfstate"
     dynamodb_table = "terraform-locks"
     region         = "ap-south-1"
