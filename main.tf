6349ba7769b81453f88baaa5b8d351c52b74c3cd
darshangammaedge <darshan.patidar@gammaedge.io>
2026-01-30 16:17:34 +0530

    make 3 buckets

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 resource "aws_s3_bucket" "my-bucket" {
   bucket = "my-demo-terraform-bucket-darshan-5"
 }
+
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "my-demo-terraform-bucket-darshan-7"
+}
+
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "my-demo-terraform-bucket-darshan-8"
+}
