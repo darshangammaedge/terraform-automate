ca3ccf0510ebf812c6a34c9d58af6c093f843cbf
darshangammaedge <darshan.patidar@gammaedge.io>
2026-01-30 12:01:18 +0530

    add dynamodb in backend

--- backend.tf
+++ backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-s3-bucket-darshan"
     key            = "dev/terraform.tfstate"
+    dynamodb_table = "terraform-locks"
     region         = "ap-south-1"
   }
 }
